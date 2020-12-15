ca57745db6c84550dafd54b801f9dad0bfdd72c5
Akash Bhave <akashbbhave@gmail.com>
2020-12-15 15:14:00 -0500

    Create officers section, make padding more responsive

---
+++ script.js
@@ -0,0 +1,40 @@
+const officers = [
+  {
+    name: "Akash Bhave",
+    role: "Co-President",
+    image: "akash.jpeg",
+    year: 2021,
+  },
+  {
+    name: "Alex Suh",
+    role: "Co-President",
+    image: "alex.jpeg",
+    year: 2021,
+  },
+  {
+    name: "Anna Hsu",
+    role: "Chief Lecturer",
+    image: "anna.jpeg",
+    year: 2022,
+  },
+  {
+    name: "Bryant Park",
+    role: "Publicist",
+    image: "bryant.png",
+    year: 2022,
+  },
+  {
+    name: "Gayatri Gopavajhala",
+    role: "Lecturer",
+    image: "gayatri.png",
+    year: 2021,
+  },
+  {
+    name: "Kari Naga",
+    role: "Lecturer",
+    image: "kari.jpeg",
+    year: 2021,
+  },
+];
+
+console.log(officers);
